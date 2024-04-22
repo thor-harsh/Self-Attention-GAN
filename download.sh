1739b060a4450d82fafb1b25b7fdf78ba78bfc8c
thor-harsh <105957576+thor-harsh@users.noreply.github.com>
2024-04-22 18:37:54 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,13 +2,17 @@ FILE=$1
 
 if [ $FILE == 'CelebA' ]
 then
-    URL=https://www.dropbox.com/s/3e5cmqgplchz85o/CelebA_nocrop.zip?dl=0
-    ZIP_FILE=./data/CelebA.zip
+#This zip which was attached to this URL so this doesn't work so we can change the URL to a new one or even add more new image datasets
+    # URL=https://www.dropbox.com/s/3e5cmqgplchz85o/CelebA_nocrop.zip?dl=0
+    # ZIP_FILE=./data/CelebA.zip
+    pass
 
 elif [ $FILE == 'LSUN' ]
 then
-    URL=https://www.dropbox.com/s/zt7d2hchrw7cp9p/church_outdoor_train_lmdb.zip?dl=0
-    ZIP_FILE=./data/church_outdoor_train_lmdb.zip
+#Same for this URL too
+    # URL=https://www.dropbox.com/s/zt7d2hchrw7cp9p/church_outdoor_train_lmdb.zip?dl=0
+    # ZIP_FILE=./data/church_outdoor_train_lmdb.zip
+    pass
 else
     echo "Available datasets are: CelebA and LSUN"
     exit 1
